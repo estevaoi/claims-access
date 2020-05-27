3870008aead15f420d7dd72d275788fb98b112c8
estevaoi <estevaonorberto@gmail.com>
2020-05-26 22:17:29 -0300

    update bug

--- claims_access.js
+++ claims_access.js
@@ -1,27 +1,24 @@
-function claims_access(needle, haystack, argStrict) {
+function claims_access(needle, haystack, argStrict = false) {
     //  discuss at: https://github.com/estevaoi/claims-access/issues
     //   example 1: in_array('application_get', ['application_post', 'application_get', 'application_put']);
     //   returns 1: true
     //   example 4: in_array('application_get', ['application_post', 'application_get-all', 'application_put'], true);
     //   returns 4: false
 
-    var key = '';
     var strict = !!argStrict;
 
-    //we prevent the double check (strict && arr[key] === ndl) || (!strict && arr[key] == ndl)
     //in just one for, in order to improve the performance
     //deciding wich type of comparation will do before walk array
     if (strict) {
-        return haystack.indexOf(claims) > -1;
+        return haystack.indexOf(needle) > -1;
     } else {
-        return haystack.filter((val) => {
+        var value = haystack.filter((val) => {
             if (val.indexOf(needle) > -1) {
                 return true;
             }
         });
+        return (value != '') ? true : false;
     }
-
-    return false;
 }
 
 module.exports = claims_access;
